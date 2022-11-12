74b5d6922e3108ec6e14a42fe5ad9495b0b4e232
Fyodor Danilov <danilovfa@protonmail.com>
2022-11-12 21:16:53 +0300

    Add check folders

--- src/index.js
+++ src/index.js
@@ -2,16 +2,49 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const changedFiles = core.getInput('changed-files');
+  // Get all changed files in string
+  const changedFilesString = core.getInput('changed-files');
+
+  // Get path
   const defaultPath = core.getInput('path');
+
+  // Get number of nested folders
+  const nesting = core.getInput('nesting');
+
+  // Split string in array
+  const changedFiles = changedFilesString.trim().split(/\s+/);
+
+  const firstPath = getFolderName(changedFiles[0], nesting);
+  console.log(`File[${0}] folder: ${firstPath}`);
+
+  let isGood = true;
+
+  for (let i = 1; i < changedFiles.length; i++) {
+    let tempPath = getFolderName(changedFiles[i], nesting);
+    console.log(`File[${i}] folder: ${tempPath}`);
+    if (tempPath != firstPath) {
+        isGood = false;
+        break;
+    }
+  }
+
+  // If folders are different throw error
+  if (isGood == false) {
+    throw new Error();
+  }
 
   console.log(changedFiles);
   console.log(defaultPath);
-
-  // Get the JSON webhook payload for the event that triggered the workflow
-  // const payload = JSON.stringify(github.context.payload, undefined, 2)
-  // console.log(`The event payload: ${payload}`);
+  
 } catch (error) {
   core.setFailed(error.message);
+}
+
+function getFolderName(file, nesting) {
+  const filePath = Object.values(file.trim().split('/'));
+  if (nesting > filePath.length) {
+      return;
+  } else {
+      return filePath[nesting]
+  }
 }
